be2e4064127ab5dcc7a6aed9f5ba4198f6918cb6
Ni0duann <2014567981@qq.com>
2025-01-20 15:01:37 +0800

    feat：简单优化下样式

--- src/App.tsx
+++ src/App.tsx
@@ -9,12 +9,11 @@ declare global {
 
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center ">
-      <h1 className="text-3xl font-bold underline">异常数据</h1>
-      <h2>前端异常</h2>
+    <div className="flex flex-col items-center justify-center space-y-8 p-8 bg-gray-100 min-h-screen">
+      <h1 className="text-4xl font-bold underline text-gray-800">前端异常</h1>
       <div className="flex flex-row space-x-4">
         <button
-          className="bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-700"
+          className="bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-700 transition duration-300 ease-in-out shadow-md"
           onClick={() => {
             if (window.someVar) {
               window.someVar.error = "error";
@@ -24,7 +23,7 @@ function App() {
           代码执行异常
         </button>
         <button
-          className="bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-700"
+          className="bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-700 transition duration-300 ease-in-out shadow-md"
           onClick={() => {
             new Promise(function (resolve, reject) {
               if (window.someVar) {
@@ -37,13 +36,13 @@ function App() {
           Promise异常
         </button>
         <button
-          className="bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-700"
+          className="bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-700 transition duration-300 ease-in-out shadow-md"
           onClick={() => console.log("bugAsset")}
         >
           静态资源加载异常
         </button>
         <button
-          className="bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-700"
+          className="bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-700 transition duration-300 ease-in-out shadow-md"
           onClick={() => {
             console.error(new Error("错误捕获"));
           }}
@@ -51,12 +50,85 @@ function App() {
           console.error 异常
         </button>
         <button
-          className="bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-700"
+          className="bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-700 transition duration-300 ease-in-out shadow-md"
           onClick={() => {
-            console.log('todo');
+            console.log("todo");
           }}
         >
           跨域异常
+        </button>
+      </div>
+      <h1 className="text-4xl font-bold underline text-gray-800">接口异常</h1>
+      <div className="flex flex-row justify-between space-x-5">
+        <button
+          className="bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-700 transition duration-300 ease-in-out shadow-md"
+          onClick={() => {
+            console.log("todo");
+          }}
+        >
+          未响应/超时响应异常
+        </button>
+        <button
+          className="bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-700 transition duration-300 ease-in-out shadow-md"
+          onClick={() => {
+            console.log("todo");
+          }}
+        >
+          4xx 请求异常
+        </button>
+        <button
+          className="bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-700 transition duration-300 ease-in-out shadow-md"
+          onClick={() => {
+            console.log("todo");
+          }}
+        >
+          fetch 请求异常
+        </button>
+      </div>
+      <h1 className="text-4xl font-bold underline text-gray-800">白屏异常</h1>
+      <div className="flex flex-row space-x-4">
+        <button
+          className="bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-700 transition duration-300 ease-in-out shadow-md"
+          onClick={() => {
+            alert("页面加载时已经监控");
+          }}
+        >
+          白屏异常
+        </button>
+      </div>
+      <h1 className="text-4xl font-bold underline text-gray-800">行为数据</h1>
+      <div className="flex flex-row space-x-4">
+        <button
+          className="bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-700 transition duration-300 ease-in-out shadow-md"
+          onClick={() => {
+            alert("页面加载时已经监控");
+          }}
+        >
+          用户设备类型，浏览器版本，webview引擎类型
+        </button>
+        <button
+          className="bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-700 transition duration-300 ease-in-out shadow-md"
+          onClick={() => {
+            alert("页面加载时已经监控");
+          }}
+        >
+          获取页面性能指标
+        </button>
+        <button
+          className="bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-700 transition duration-300 ease-in-out shadow-md"
+          onClick={() => {
+            alert("页面加载时已经监控");
+          }}
+        >
+          获取页面性能指标
+        </button>
+        <button
+          className="bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-700 transition duration-300 ease-in-out shadow-md"
+          onClick={() => {
+            alert("页面加载时已经监控");
+          }}
+        >
+          点击事件
         </button>
       </div>
     </div>
